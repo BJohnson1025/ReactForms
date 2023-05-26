d767a6b0a2e01269d3abba2b27c1ebdcfac8eb9d
Jahbari Tesfaye <yveri95@gmail.com>
2023-05-25 20:57:56 -0400

    Controlled

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { useState } from "react";
 
 const Form = () => {
+  const [username, setUsername] = useState(''); 
+  const handleChange = (event) => {
+      setUsername(event.target.value)
+    }
+  const handleSubmit = (event) => {
+      event.preventDefault()
+      console.log(username)
+      setUsername('')
+    }
+
   return (
     <div id="container">
       <div id="navbar">Form.js</div>
       <form onSubmit={handleSubmit}>
         <label htmlFor="username">Username:</label>
-        <input type="text" name="username" />
+        <input type='text' name='username' value={username} onChange={handleChange} />
         <button type="submit">Submit</button>
       </form>
     </div>
   );
 };
-const handleSubmit = (event) => {
-  event.preventDefault();
-  const username = event.target.username.value;
-  console.log(username);
-};
+
 
 ReactDOM.render(<Form />, document.getElementById("app"));
