b4ebfab05f42640f46592e4747ed0323ce433d34
Brandon Johnson <developmentbybjohnson@gmail.com>
2023-05-25 19:06:01 -0500

    Uncontrolled

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 const Form = () => {
   return (
-    <div id='container'>
-      <div id='navbar'>
-        Form.js
-      </div>
-      <form>
-        {/* your form fields here */}
+    <div id="container">
+      <div id="navbar">Form.js</div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username:</label>
+        <input type="text" name="username" />
+        <button type="submit">Submit</button>
       </form>
     </div>
-  )
-}
+  );
+};
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const username = event.target.username.value;
+  console.log(username);
+};
 
-
-ReactDOM.render(
-  <Form />,
-  document.getElementById('app')
-)
+ReactDOM.render(<Form />, document.getElementById("app"));
